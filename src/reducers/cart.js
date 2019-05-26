fd8df86261df80cbc46044c7546e63f733107a84
Dorio Cioran <dorio@anthood.com>
2019-05-26 10:49:20 +0200

    fix cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -7,6 +7,10 @@ const initialState = {
     }
 }
 
+const isObject = (a) => {
+    return (!!a) && (a.constructor === Object);
+};
+
 const cartItems = (state = initialState, action) => {
     switch (action.type) {
         // case 'ADD_TO_CART':
@@ -17,9 +21,20 @@ const cartItems = (state = initialState, action) => {
         //     }
         case types.GET_CART_SUCCESS:
             // console.log(action.response);
+            // console.log(isObject(action.response));
+
+            // let cartData = initialState.data;
+
+            // if (![].isArray(action.response) || !array.length) {
+            //     // array does not exist, is not an array, or is empty
+            //     // ⇒ do not attempt to process array
+            //   }
+
+            const cartResponse = ( isObject(action.response) ? action.response : initialState.data );
+
             return {
                 ...state,
-                data: action.response
+                data: cartResponse
             }    
             // break;
         default:
